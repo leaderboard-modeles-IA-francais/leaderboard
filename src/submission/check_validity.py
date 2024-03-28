9f4d66ffafb9aeff516bf1fe15a8549091f78444
Clémentine <cle.fourrier@gmail.com>
2024-03-28 15:01:13 +0100

    fixed error message

--- src/submission/check_validity.py
+++ src/submission/check_validity.py
@@ -62,7 +62,7 @@ def is_model_on_hub(model_name: str, revision: str, token: str = None, trust_rem
     except Exception as e:
         if "You are trying to access a gated repo." in str(e):
             return True, "uses a gated model.", None
-        return False, "was not found on hub!", None
+        return False, f"was not found or misconfigured on the hub! Error raised was {e.args[0]}", None
 
 def get_model_size(model_info: ModelInfo, precision: str):
     size_pattern = re.compile(r"(\d+\.)?\d+(b|m)")
